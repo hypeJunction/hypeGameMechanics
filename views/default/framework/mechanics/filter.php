89879a45c69378c3eff68f125730e5f47a1a5182
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-03-25 14:13:34 +0100

    Use pagehandler constant

--- views/default/framework/mechanics/filter.php
+++ views/default/framework/mechanics/filter.php
@@ -7,13 +7,13 @@ $filter_context = elgg_extract('filter_context', $vars, 'leaderboard');
 $tabs = array(
 	'leaderboard' => array(
 		'text' => elgg_echo('mechanics:leaderboard'),
-		'href' => "points/leaderboard",
+		'href' => PAGEHANDLER . "/leaderboard",
 		'selected' => ($filter_context == 'leaderboard'),
 		'priority' => 100,
 	),
 	'badges' => array(
 		'text' => elgg_echo('mechanics:badges:site'),
-		'href' => "points/badges",
+		'href' => PAGEHANDLER . "/badges",
 		'selected' => ($filter_context == 'badges'),
 		'priority' => 200,
 	),
@@ -24,13 +24,13 @@ if (elgg_is_logged_in()) {
 	$user = elgg_get_logged_in_user_entity();
 	$tabs['owner'] = array(
 		'text' => elgg_echo('mechanics:badges:mine'),
-		'href' => "points/owner/$user->username",
+		'href' => PAGEHANDLER . "/owner/$user->username",
 		'selected' => ($filter_context == 'owner'),
 		'priority' => 300
 	);
 	$tabs['history'] = array(
 		'text' => elgg_echo('mechanics:history'),
-		'href' => "points/history/$user->username",
+		'href' => PAGEHANDLER . "/history/$user->username",
 		'selected' => ($filter_context == 'history'),
 		'priority' => 400,
 	);
